a1c273ec520607c46c32450503d81b7c750eecac
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-07-04 21:38:04 +0200

    Single country fetch

--- src/pages/SingleCountry.js
+++ src/pages/SingleCountry.js
@@ -1,9 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+const url = 'https://restcountries.eu/rest/v2/name/';
 
 const SingleCountry = () => {
+  const { id } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    async function getCountry () {
+      try {
+        const response = await fetch(`${url}${id}`);
+        const data = await response.json();
+        console.log(data);
+        if (data) {
+          const {
+            name: name,
+          } = data[0];
+
+          const newCountry = {
+            name
+          }
+          setCountry(newCountry);
+        } else {
+          setCountry(null);
+        }
+        setLoading(false);
+      } catch (err) {
+        console.log(err);     
+        setLoading(false);
+      }
+    }
+    getCountry();
+  }, [id]); 
+
+  if (loading) {
+  }
+  if (!country) {
+    return <h2 className="section-title">no cocktail to display</h2>
+  }
+
+  const {name} = country;
+
   return (
     <div>
-      country page
+      {name}
     </div>
   );
 }
