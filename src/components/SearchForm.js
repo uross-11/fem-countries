1c8a8b36f28ed980f160ffbc31b25b4b99e571f9
Uros Ignjatovic <u.ignjatovic00@gmail.com>
2021-07-05 14:26:27 +0200

    Add form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -3,7 +3,15 @@ import React from 'react';
 const SearchForm = () => {
   return (
     <form>
-      
+      <input type="text" placeholder='Search for a country...' name="name" id="name" />
+      <select name="region" id="region">
+        <option value="" selected disabled hidden>Filter by region</option>
+        <option value="africa">Africa</option>
+        <option value="america">America</option>
+        <option value="asia">Asia</option>
+        <option value="europe">Europe</option>
+        <option value="oceania">Oceania</option>
+      </select>
     </form>
   );
 }
